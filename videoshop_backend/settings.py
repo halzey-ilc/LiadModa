c4018eff23ac6e896eaad999f5c179857406a3b6
timmbaland <talantbekadik135@gmail.com>
2025-05-18 02:36:33 +0600

    fix: remove duplicate SECRET_KEY

--- videoshop_backend/settings.py
+++ videoshop_backend/settings.py
@@ -20,7 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-1b)a*_@zn0uuh2fdfc798ltdcg$g4nizogi!-7emf)trb8p8i#"
 
 SECRET_KEY = os.environ.get("SECRET_KEY", "django-insecure-1b)a*_@zn0uuh2fdfc798ltdcg$g4nizogi!-7emf)trb8p8i#")
 
